24da478ae3dc4b0088109dc69f1c8e5b9d36aa1b
Vladimir Nitochkin <vovnit@gmail.com>
2020-08-25 22:29:44 +0300

    init project

---
+++ StoryboardWorkshop/StoryboardWorkshop.h
@@ -0,0 +1,19 @@
+//
+//  StoryboardWorkshop.h
+//  StoryboardWorkshop
+//
+//  Created by Vladimir Nitochkin on 25.08.2020.
+//  Copyright © 2020 Vladimir Nitochkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoryboardWorkshop.
+FOUNDATION_EXPORT double StoryboardWorkshopVersionNumber;
+
+//! Project version string for StoryboardWorkshop.
+FOUNDATION_EXPORT const unsigned char StoryboardWorkshopVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoryboardWorkshop/PublicHeader.h>
+
+
